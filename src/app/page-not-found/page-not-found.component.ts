896c28680660296792f231bb9d0316e3c76cf881
Edric Chan <edric.chan.1997@gmail.com>
2017-10-18 23:40:15 +0800

    chore: Add page for when url is invalid

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styles: []
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
