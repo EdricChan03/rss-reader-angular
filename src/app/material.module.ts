22c84cdbda2b9a49aa2f5f5a104fc551ed3676e8
Edric Chan <edric.chan.1997@gmail.com>
2017-10-18 23:44:38 +0800

    refactor(MaterialModule): Import modules from separate locations

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,31 +1,28 @@
 // Material design.
-import {
-	MatCardModule,
-	MatToolbarModule,
-	MatButtonModule,
-	MatIconModule,
-	MatProgressBarModule,
-	MatDialogModule,
-	MatMenuModule,
-	MatCheckboxModule,
-	MatSelectModule,
-	MatChipsModule,
-	MatSidenavModule,
-	MatTooltipModule,
-	MatFormFieldModule,
-	MatSnackBarModule,
-	MatProgressSpinnerModule,
-	MatSlideToggleModule,
-	MatInputModule,
-	MatListModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 const MATERIAL_MODULES = [
 	MatCardModule,
 	MatToolbarModule,
 	MatButtonModule,
 	MatIconModule,
-	MatProgressBarModule,
 	MatDialogModule,
 	MatMenuModule,
 	MatCheckboxModule,
@@ -38,8 +35,9 @@ const MATERIAL_MODULES = [
 	MatProgressSpinnerModule,
 	MatSlideToggleModule,
 	MatInputModule,
-	MatListModule
-]
+	MatListModule,
+	MatRadioModule
+];
 @NgModule({
 	imports: [
 		MATERIAL_MODULES
@@ -48,4 +46,4 @@ const MATERIAL_MODULES = [
 		MATERIAL_MODULES
 	]
 })
-export class MaterialModule {}
+export class MaterialModule { }
