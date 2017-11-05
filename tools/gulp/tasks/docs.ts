daf113cf1a2f1a0e48fa71c878d291ed07ac2024
Edric Chan <edric.chan.1997@gmail.com>
2017-11-05 16:43:40 +0800

    feat: Add gulp to project - Ignore `src/assets/docs` - :x: BREAKING CHANGE: Generate docs by running `gulp docs` - Implement generation of docs

---
+++ tools/gulp/tasks/docs.ts
@@ -0,0 +1,85 @@
+import { task, src, dest } from 'gulp';
+import * as path from 'path';
+
+const markdown = require('gulp-markdown');
+const transform = require('gulp-transform');
+const hljs = require('highlight.js');
+
+const markdownOptions = {
+	// Add syntax highlight using highlight.js
+	highlight: (code: string, language: string): string => {
+		if (language) {
+			// highlight.js expects "typescript" written out, while Github supports "ts".
+			let lang = language.toLowerCase() === 'ts' ? 'typescript' : language;
+			return hljs.highlight(lang, code).value;
+		}
+
+		return code;
+	}
+};
+task('docs', ['docs-guides', 'docs-dev', 'docs-img']);
+
+task('docs-guides', () => {
+	return src('docs/guides/**/!(README.md)')
+		.pipe(markdown(markdownOptions))
+		.pipe(transform('utf8', transformMarkdownFiles))
+		.pipe(dest('src/assets/docs/guides'));
+});
+task('docs-dev', () => {
+	return src('docs/dev/**/!(README.md)')
+		.pipe(markdown(markdownOptions))
+		.pipe(transform('utf8', transformMarkdownFiles))
+		.pipe(dest('src/assets/docs/dev'));
+});
+task('docs-img', ()=> {
+	return src('docs/img/**')
+		.pipe(dest('src/assets/docs/img'));
+})
+
+/**
+ * Transforms the Markdown files
+ * @param {Buffer} buffer The buffer of the file
+ */
+function transformMarkdownFiles(content: string, file: any): Buffer | string {
+
+	// Markdown files can contain links to other markdown files.
+	// Most of those links don't work in the Material docs, because the paths are invalid in the
+	// documentation page. Using a RegExp to rewrite links in HTML files to work in the docs.
+	const LINK_PATTERN = /(<a[^>]*) href="([^"]*)"/g;
+	const IMG_PATTERN = /(<img[^>]*) src="([^"]*)"/g;
+	// Replace the URL in anchor elements inside of compiled markdown files.
+	content = content.replace(LINK_PATTERN, (_match: string, head: string, link: string) =>
+		// The head is the first match of the RegExp and is necessary to ensure that the RegExp matches
+		// an anchor element. The head will be then used to re-create the existing anchor element.
+		// If the head is not prepended to the replaced value, then the first match will be lost.
+		`${head} href="${fixMarkdownDocLinks(link, file.path)}"`
+	);
+	content = content.replace(IMG_PATTERN, (_match: string, head: string, link: string) =>
+		// The head is the first match of the RegExp and is necessary to ensure that the RegExp matches
+		// an anchor element. The head will be then used to re-create the existing anchor element.
+		// If the head is not prepended to the replaced value, then the first match will be lost.
+		`${head} src="${fixMarkdownDocImgs(link)}"`
+	);
+	content = content.replace(':arrow_left:', '⬅️');
+	return content;
+};
+
+function fixMarkdownDocLinks(link: string, filePath: string): string {
+	// As for now, only markdown links that are relative and inside of the guides/ directory
+	// will be rewritten.
+	if (!filePath.includes(path.normalize('guides/')) || link.startsWith('http')) {
+		return link;
+	}
+	
+	
+	let baseName = path.basename(link, path.extname(link));
+
+	// Temporary link the file to the /guide URL because that's the route where the
+	// guides can be loaded in the Material docs.
+	return `doc/guides/${baseName}`;
+}
+function fixMarkdownDocImgs(link: string): string {
+	link = link.replace('..', 'assets/docs')
+	return link;
+}
+process.on('unhandledRejection', up => { throw up })
