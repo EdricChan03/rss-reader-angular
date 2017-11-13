796be66c1b626344c329f739ff98ed0e0b455aee
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:37:24 +0800

    refactor(guide-viewer): Remove console logging

--- src/app/pages/guide-viewer/guide-viewer.component.ts
+++ src/app/pages/guide-viewer/guide-viewer.component.ts
@@ -12,13 +12,9 @@ export class GuideViewer {
 	constructor(private route: ActivatedRoute,
 		private router: Router, private guideItems: GuideItems) {
 		route.params.subscribe(result => {
-			console.log(result);
-			console.log(result.docTypeId);
 			if (result.docTypeId == "guides") {
-				console.log("Guide doc");
 				this.docUrl = guideItems.getGuideItemById(result.docId).document;
 			} else if (result.docTypeId == "dev") {
-				console.log("Dev doc");
 				this.docUrl = guideItems.getDevItemById(result.docId).document;
 			} else {
 				console.warn("Invalid URL. Heading back to docs list page..");
