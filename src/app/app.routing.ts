419ec7c0d36565d7c7445bee064f62c297c774ed
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:39:25 +0800

    chore(routing): Add components to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,29 @@
+import { GuideViewer } from './pages/guide-viewer/guide-viewer.component';
+import { GuidesList } from './pages/guide-list/guide-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TestpageComponent } from './testpage/testpage.component';
 import { SettingsComponent } from './settings/settings.component';
 import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AndroidComponent } from './devices/android/android.component';
+import { IOSComponent } from './devices/ios/ios.component';
 
 const APP_ROUTES: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'settings', component: SettingsComponent },
 	{ path: 'test', component: TestpageComponent },
-	{ path: '', pathMatch: 'full', redirectTo: '/home'},
-	{ path: '**', component: PageNotFoundComponent}
+	{ path: 'docs', component: GuidesList },
+	{ path: 'doc/:docTypeId/:docId', component: GuideViewer },
+	{
+		path: 'devices', children: [
+			{ path: 'android', component: AndroidComponent },
+			{ path: 'ios', component: IOSComponent },
+			{ path: '**', redirectTo: '/devices/android' }
+		]
+	},
+	{ path: '', pathMatch: 'full', redirectTo: '/home' },
+	{ path: '**', component: PageNotFoundComponent }
 ];
 
 export const AppRouting: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
