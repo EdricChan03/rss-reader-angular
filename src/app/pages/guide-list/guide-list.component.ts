161ee5a6306de14edece2ffca8f31cafe030d75d
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:37:06 +0800

    feat(guide-list): Add a list of guides

--- src/app/pages/guide-list/guide-list.component.ts
+++ src/app/pages/guide-list/guide-list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GuideItem, GuideItems } from '../../guide-items';
 
 @Component({
 	selector: 'guide-list',
 	templateUrl: 'guide-list.component.html'
 })
-export class GuidesList {
+export class GuidesList implements OnInit {
+	constructor(private _guideItems: GuideItems){}
+	guideItems: GuideItem[];
+	devItems: GuideItem[];
+	ngOnInit() {
+		this.guideItems = this._guideItems.getGuideItems();
+		this.devItems = this._guideItems.getDevItems();
+	}
 }
