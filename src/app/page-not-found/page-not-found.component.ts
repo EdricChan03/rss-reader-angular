7334f2a89c85fbedf94ad8961c4a5a7e82617291
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:30:11 +0800

    feat(page-not-found): Show warning icon - Also shows the invalid URL

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,15 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styles: []
+	selector: 'app-page-not-found',
+	templateUrl: './page-not-found.component.html',
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+	constructor(public router: Router) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
