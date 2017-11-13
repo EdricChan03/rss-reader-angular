915f8b1913995fe47e06874cb5c8126e2f565565
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:28:57 +0800

    feat: Add iOS and Android :warning: COMING SOON

---
+++ src/app/devices/android/android.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-android',
+	templateUrl: './android.component.html'
+})
+export class AndroidComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
---
+++ src/app/devices/ios/ios.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-ios',
+	templateUrl: './ios.component.html'
+})
+export class IOSComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
