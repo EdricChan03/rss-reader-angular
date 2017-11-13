cc554af2a74b13d8bd893027ee7aada23e74da14
Edric Chan <edric.chan.1997@gmail.com>
2017-11-13 22:30:33 +0800

    chore(guide-items): Add missing `url` property

--- src/app/guide-items.ts
+++ src/app/guide-items.ts
@@ -4,45 +4,54 @@ export interface GuideItem {
 	id: string;
 	name: string;
 	document: string;
+	url: string;
 }
 
 const GUIDES: GuideItem[] = [
 	{
 		id: "getting-started",
 		document: "assets/docs/guides/getting-started.html",
+		url: "doc/guides/getting-started",
 		name: "Getting Started"
 	},
 	{
 		id: "generate-api-key",
 		document: "assets/docs/guides/generate-api-key.html",
+		url: "doc/guides/generate-api-key",
 		name: "Generating an API Key"
 	},
 	{
 		id: "troubleshooting",
 		document: "assets/docs/guides/troubleshooting.html",
+		url: "doc/guides/troubleshooting",
 		name: "Troubleshooting"
 	}
 ];
 const DEV_GUIDES: GuideItem[] = [
 	{
 		id: "coding-standards",
-		document: "assets/docs/guides/shared-injectable.html",
+		document: "assets/docs/dev/coding-standards.html",
+		url: "doc/dev/coding-standards",
 		name: "Coding standards"
 	},
 	{
 		id: "contributing",
-		document: "assets/docs/guides/contributing.html",
+		document: "assets/docs/dev/contributing.html",
+		url: "doc/dev/contributing",
 		name: "Contributing"
 	},
 	{
 		id: "shared-injectable",
-		document: "assets/docs/guides/shared-injectable.html",
-		name: "Shared (Injectable)"
+		document: "assets/docs/dev/shared-injectable.html",
+		url: "doc/dev/shared-injectable",
+		name: "SharedInjectable (Injectable)"
 	}
 ]
 @Injectable()
 export class GuideItems {
-
+	getAllItems(): GuideItem[] {
+		return GUIDES.concat(DEV_GUIDES);
+	}
 	getGuideItems(): GuideItem[] {
 		return GUIDES;
 	}
