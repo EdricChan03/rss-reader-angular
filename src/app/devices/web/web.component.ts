60e501e78947873d7f6d84ef03e84b9b5623706d
Edric Chan <edric.chan.1997@gmail.com>
2017-12-12 10:24:20 +0800

    feat: Add subpage for a web app

---
+++ src/app/devices/web/web.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-web',
+	templateUrl: './web.component.html',
+	styleUrls: ['web.component.scss']
+})
+export class WebComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
