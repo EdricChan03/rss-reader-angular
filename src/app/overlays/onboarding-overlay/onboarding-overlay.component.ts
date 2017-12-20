ea4cdd155d63e451563e46d475e38a8954c6fae2
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 01:21:46 +0800

    feat: Add onboarding overlay

---
+++ src/app/overlays/onboarding-overlay/onboarding-overlay.component.ts
@@ -0,0 +1,87 @@
+import { OverlayService } from '../../overlay.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { trigger, style, animate, transition, state } from '@angular/animations';
+
+@Component({
+	selector: 'app-onboarding-overlay',
+	templateUrl: './onboarding-overlay.component.html',
+	styleUrls: ['./onboarding-overlay.component.scss'],
+	animations: [
+		trigger(
+			'enterAnimation', [
+				state('enter', style({ transform: 'scale(0)', opacity: 0 })),
+				state('leave', style({ transform: 'scale(1)', opacity: 1 })),
+				transition('enter <=> leave', animate('200ms ease-in-out'))
+			]
+		)
+	]
+})
+export class OnboardingOverlayComponent {
+	index: 0 | 1 | 2 | 3 = 0;
+	content = [
+		{
+
+		},
+		{
+			color: '#FFC107',
+			content: 'Multiple news sources. One reader.',
+			imgSrc: '/assets/img/News Article.png'
+		},
+		{
+			color: '#FF5722',
+			content: 'Customizable. Tons of options.',
+			imgSrc: '/assets/img/Customizing UI.png'
+		},
+		{
+			color: '#F06292',
+			content: 'Many news sources to explore!',
+			imgSrc: '/assets/img/Explore News Sources.png'
+		}];
+
+	constructor(
+		private router: Router,
+		private overlayService: OverlayService
+	) { }
+	get nextFABIcon(): string {
+		if (this.index === 3) {
+			return 'done';
+		} else {
+			return 'arrow_forward';
+		}
+	}
+	get nextFABColor(): 'primary' | 'accent' {
+		if (this.index === 3) {
+			return 'accent';
+		} else {
+			return 'primary';
+		}
+	}
+	get nextFABTooltip(): string {
+		if (this.index === 3) {
+			return 'Done';
+		} else {
+			return 'Next';
+		}
+	}
+	prevIndex() {
+		if (this.index !== 0) {
+			this.index--;
+		}
+	}
+	nextIndex() {
+		if (this.index !== 3) {
+			this.index++;
+		} else {
+			this.gettingStarted();
+		}
+	}
+	goToIndex(index: 0 | 1 | 2 | 3) {
+		console.log(index);
+		this.index = index;
+	}
+	gettingStarted() {
+		this.router.navigate(['/home']);
+		this.overlayService.close();
+	}
+}
