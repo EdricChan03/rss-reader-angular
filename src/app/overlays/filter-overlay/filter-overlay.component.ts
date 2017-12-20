7d8ffeea53c993c1ef989158976cdbe2db6fb09b
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 00:43:13 +0800

    feat: Add explore page

---
+++ src/app/overlays/filter-overlay/filter-overlay.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-filter-overlay',
+	templateUrl: './filter-overlay.component.html',
+})
+export class FilterOverlayComponent {
+	opts = {
+		domains: '',
+		langCode: '',
+		sortBy: 'popularity'
+	};
+}
