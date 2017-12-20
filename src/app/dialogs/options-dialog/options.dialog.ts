0e609ede974fcd77f2bb6e609e43554a26276e53
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 00:43:33 +0800

    lint: Update linting accordingly

--- src/app/dialogs/options-dialog/options.dialog.ts
+++ src/app/dialogs/options-dialog/options.dialog.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 	selector: 'options-dialog',
 	templateUrl: './options.dialog.html'
 })
+// tslint:disable-next-line:component-class-suffix
 export class OptionsDialog implements OnInit {
-	constructor(private dialogRef: MatDialogRef<OptionsDialog>, private shared: SharedInjectable, private dialog: MatDialog){}
+	constructor(private dialogRef: MatDialogRef<OptionsDialog>, private shared: SharedInjectable, private dialog: MatDialog) { }
+	// tslint:disable-next-line:member-ordering
 	options: FeedOptions;
+	// tslint:disable-next-line:member-ordering
 	tempOptions: FeedOptions;
 	cancel() {
 		this.dialogRef.close();
@@ -17,20 +20,20 @@ export class OptionsDialog implements OnInit {
 	save() {
 		this.dialogRef.close();
 		window.localStorage.setItem('feedOptions', JSON.stringify(this.options));
-		let snackBarRef = this.shared.openSnackBarWithRef({msg: "Options saved", action: "Undo"})
-		snackBarRef.onAction().subscribe(_ => {
+		const snackBarRef = this.shared.openSnackBarWithRef({ msg: 'Options saved', action: 'Undo' });
+		snackBarRef.onAction().subscribe(() => {
 			this.options = this.tempOptions;
 			this.dialog.open(OptionsDialog);
-		})
+		});
 	}
 	ngOnInit() {
 		if (window.localStorage.getItem('feedOptions')) {
-			this.options = <FeedOptions> JSON.parse(window.localStorage.getItem('feedOptions'));
-			this.tempOptions = <FeedOptions> JSON.parse(window.localStorage.getItem('feedOptions'));
+			this.options = <FeedOptions>JSON.parse(window.localStorage.getItem('feedOptions'));
+			this.tempOptions = <FeedOptions>JSON.parse(window.localStorage.getItem('feedOptions'));
 		} else {
 			this.options = {
 				amount: 20
-			}
+			};
 		}
 	}
 }
