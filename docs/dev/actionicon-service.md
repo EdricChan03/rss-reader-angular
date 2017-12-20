97e015f0d8f6611f31d66bed532e11221adfedf5
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 00:41:55 +0800

    feat: Add ActionIconService - For more info, check out the new docs!

---
+++ docs/dev/actionicon-service.md
@@ -0,0 +1,185 @@
+# `ActionIconService`
+## Description
+This `@Injectable()` makes it easier to add menu items to a toolbar, similar to like Android. (For those of you who don't know, those icons on that toolbar are known as menu item just shown as action buttons and the toolbar's also known as an `ActionBar`.) Anyways, enough of Android and Material Design and whatever.
+In the future, this will be revisited and may be integrated with `SharedInjectable`.
+
+## Notices
+Ensure that `ActionIconsModule` is imported to your main app's module under the `imports` array:
+```typescript
+import { ActionIconsModule } from 'path/to/actionitem.service';
+@NgModule({
+	imports: [ActionIconsModule]
+})
+```
+That's it! There's nothing else there is! On to the documentation then...
+
+(Examples will be at the bottom this time...)
+
+# Methods
+## `addActionIcon`
+```typescript
+addActionIcon(actionItem: ActionIcon): void;
+```
+
+<!-- start-enclose-content -->
+### Description
+Adds a menu item.
+
+### Params
+Param | Type | Description | Notes
+---|---|---|---
+`actionItem` | `ActionIcon` | The menu item to add | -
+<!-- end-enclose-content -->
+
+## `getActionIcons`
+```typescript
+getActionIcons(): ActionIcon[];
+```
+
+<!-- start-enclose-content -->
+### Description
+Gets the menu items.
+
+### Returns
+`ActionIcon[]`
+<!-- end-enclose-content -->
+
+## `removeActionIcon`
+```typescript
+removeActionIcon(id: number): void;
+```
+
+<!-- start-enclose-content -->
+### Description
+Removes the specified menu item at the index.
+
+### Params
+Param | Type | Description | Notes
+---|---|---|---
+`id` | `number` | The index of the menu item | -
+<!-- end-enclose-content -->
+
+## `removeActionIcons`
+```typescript
+removeActionIcons(): void;
+```
+<!-- start-enclose-content -->
+### Description
+Removes all menu items.
+<!-- end-enclose-content -->
+
+## `getActionIconById`
+```typescript
+getActionIconById(id: number): ActionIcon;
+```
+<!-- start-enclose-content -->
+### Description
+Gets a menu item by its index.
+
+### Returns
+`ActionIcon`
+
+### Params
+Param | Type | Description | Notes
+---|---|---|---
+`id` | `number` | The index of the menu item to get | -
+<!-- end-enclose-content -->
+
+## `addActionIconOnClickListener`
+```typescript
+addActionIconOnClickListener(id: number, callback: (ev?: Event) => void): void;
+```
+<!-- start-enclose-content -->
+### Description
+Adds an onclick listener to the specified menu item.
+### Returns
+`ActionIcon`
+
+### Params
+Param | Type | Description | Notes
+---|---|---|---
+`id` | `number` | The index of the menu item to get | -
+`callback` | `(ev?: Event)=> void` | The callback of the onclick listener | -
+<!-- end-enclose-content -->
+
+# Examples
+<!-- start-enclose-content -->
+## Setting up the toolbar with the service (method 1)
+`my-app.component.html`:
+```html
+<mat-toolbar>
+	<app-actionicons></app-actionicons>
+</mat-toolbar>
+```
+## Setting up the toolbar with the service (method 2)
+See the source code for [`app.component.html`](https://github.com/Chan4077/rss-reader/blob/src/app/app.component.html).
+
+Snippet of code:
+
+```typescript
+/**
+ * Gets the menu items (getter)
+ * @returns {ActionIcon[]} The menu items
+ */
+get actionItems: ActionIcon[] {
+	return this.actionItemService.getActionIcons();
+}
+```
+```html
+<div id="more-btns" *ngFor="let actionItem of actionItems">
+	<button mat-icon-button *ngIf="actionItem.showAsAction && actionItem.href == null" (click)="actionItem.onClickListener($event)" [matTooltip]="actionItem.title">
+		<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+	</button>
+	<a mat-icon-button *ngIf="actionItem.showAsAction && actionItem.href" [href]="actionItem.href" [matTooltip]="actionItem.title">
+		<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+	</a>
+	<a mat-icon-button *ngIf="actionItem.showAsAction && actionItem.routerLink" [routerLink]="[actionItem.routerLink]" [matTooltip]="actionItem.title">
+		<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+	</a>
+</div>
+<button mat-icon-button *ngIf="showMoreMenu" [matMenuTriggerFor]="moreMenu">
+	<mat-icon>more_vert</mat-icon>
+</button>
+<mat-menu #moreMenu="matMenu">
+	<div *ngFor="let actionItem of actionItems">
+		<button mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.href == null">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+			{{actionItem.title}}
+		</button>
+		<a mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.href" [href]="actionItem.href">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+			{{actionItem.title}}
+		</a>
+		<a mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.routerLink" [routerLink]="[actionItem.routerLink]">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+			{{actionItem.title}}
+		</a>
+	</div>
+</mat-menu>
+```
+## Adding menu items
+```typescript
+export class MyComponent implements OnInit {
+	constructor(private actionItemService: ActionIconService){}
+	ngOnInit() {
+		this.actionItemService.addActionIcon({title: "Reload", icon: "refresh", onClickListener: (ev: Event)=> {
+			console.log("Reload was clicked");
+			window.location.reload(true);
+		}})
+	}
+}
+```
+## Adding onclick listeners to a menu item
+```typescript
+export class MyComponent implements OnInit {
+	constructor(private actionItemService: ActionIconService){}
+	ngOnInit() {
+		this.actionItemService.addActionIcon({title: "Open sidenav", icon: "menu"});
+		this.actionItemService.addActionIconOnClickListener(0, (ev: Event)=> {
+			console.log("Clicked");
+		})
+	}
+}
+```
+
+<!-- end-enclose-content -->
---
+++ src/app/actionitem.service.ts
@@ -0,0 +1,215 @@
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Injectable, Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Injectable()
+/**
+ * A menu item service for the toolbar.
+ */
+export class ActionIconService {
+
+	private actionItems: ActionIcon[] = [];
+	/**
+	 * Adds a menu item
+	 * @param {ActionIcon} actionItem The menu item
+	 */
+	addActionIcon(actionItem: ActionIcon) {
+		this.actionItems.push(actionItem);
+	}
+	/**
+	 * Gets menu items
+	 * @returns {ActionIcon[]}
+	 */
+	getActionIcons(): ActionIcon[] {
+		return this.actionItems;
+	}
+	/**
+	 * An alias of {@link ActionIconService#removeActionIconByIndex} to prevent apps from breaking
+	 */
+	removeActionIcon(id: number) {
+		this.removeActionIconByIndex(id);
+	}
+	/**
+	 * Removes a menu item
+	 * @param {number} id The index of the menu item to remove
+	 */
+	removeActionIconByIndex(id: number) {
+		try {
+			this.actionItems.splice(id, 1);
+		} catch (e) {
+			this.throwIdNotFoundError(id);
+		}
+	}
+	/**
+	 * Removes an action item by its title
+	 */
+	removeActionItemByTitle(title: string) {
+		try {
+			this.actionItems.splice(
+				this.actionItems.findIndex((actionIcon: ActionIcon) => {
+					return actionIcon.title == title;
+				}), 1);
+		} catch (e) {
+			this.throwTitleNotFoundError(title);
+		}
+	}
+	/**
+	 * Removes all menu items
+	 */
+	removeActionIcons() {
+		this.actionItems = [];
+	}
+	/**
+	 * Gets a menu item by its index
+	 * @param {number} id The index of the menu item to get
+	 */
+	getActionIconById(id: number): ActionIcon {
+		try {
+			return this.actionItems[id];
+		} catch (e) {
+			this.throwIdNotFoundError(id);
+		}
+	}
+	/**
+	 * Adds a menu item on click listener to the specified menu item
+	 * Note: This can also be declared manually
+	 * @param {number} id The id of the menu item
+	 * @param {Function} callback The callback when the menu item is clicked (has to be arrow function)
+	 */
+	addActionIconOnClickListener(id: number, callback: (ev?: Event) => void) {
+		try {
+			this.actionItems[id].onClickListener = callback;
+		} catch (e) {
+			this.throwIdNotFoundError(id);
+		}
+	}
+	/**
+	 * Throws an error where the id couldn't be found
+	 * @private
+	 */
+	private throwIdNotFoundError(id: number) {
+		throw new Error(`Could not find a menu item with index ${id}`);
+	}
+	/**
+	 * Throws an error where the title couldn't be found
+	 */
+	private throwTitleNotFoundError(title: string) {
+		throw new Error(`Could not find a menu item with title ${title}`);
+	}
+}
+
+/**
+ * A menu item
+ */
+export class ActionIcon {
+	/**
+	 * The title of the menu item
+	 */
+	title: string;
+	/**
+	 * The icon of the menu item
+	 */
+	icon?: string;
+	/**
+	 * The href of the menu item
+	 * NOTE: If `routerLink` is specified, don't use `href`.
+	 */
+	href?: string;
+	/**
+	 * Similar to what Android does.
+	 */
+	showAsAction?: boolean;
+	/**
+	 * The router link of the menu item
+	 * NOTE: If `href` is specified, don't use `routerLink`.
+	 */
+	routerLink?: string;
+	/**
+	 * The on click listener of the menu item
+	 * Note: This can also be set by {@link ActionIconService#addActionIconOnClickListener}
+	 */
+	onClickListener?: (ev?: Event) => void;
+	/**
+	 * The submenu of the menu item
+	 */
+	subMenu?: ActionIcon[];
+
+	constructor(title: string, icon?: string, href?: string, showAsAction?: boolean, routerLink?: string, onClickListener?: (ev?: Event) => void, subMenu?: ActionIcon[]) { }
+}
+
+@Component({
+	selector: 'app-actionicons',
+	template: `
+	<span id="more-btns" *ngFor="let actionItem of actionItems">
+		<button mat-icon-button *ngIf="actionItem.showAsAction && actionItem.href == null" (click)="actionItem.onClickListener($event)" [matTooltip]="actionItem.title">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+		</button>
+		<a mat-icon-button *ngIf="actionItem.showAsAction && actionItem.href" [href]="actionItem.href" [matTooltip]="actionItem.title">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+		</a>
+		<a mat-icon-button *ngIf="actionItem.showAsAction && actionItem.routerLink" [routerLink]="[actionItem.routerLink]" [matTooltip]="actionItem.title">
+			<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+		</a>
+	</span>
+	<button mat-icon-button *ngIf="showMoreMenu" [matMenuTriggerFor]="moreMenu">
+		<mat-icon>more_vert</mat-icon>
+	</button>
+	<mat-menu #moreMenu="matMenu">
+		<span *ngFor="let actionItem of actionItems">
+			<button mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.href == null">
+				<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+				{{actionItem.title}}
+			</button>
+			<a mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.href" [href]="actionItem.href">
+				<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+				{{actionItem.title}}
+			</a>
+			<a mat-menu-item *ngIf="!actionItem.showAsAction && actionItem.routerLink" [routerLink]="[actionItem.routerLink]">
+				<mat-icon *ngIf="actionItem.icon">{{actionItem.icon}}</mat-icon>
+				{{actionItem.title}}
+			</a>
+		</span>
+	</mat-menu>
+	
+				`
+
+})
+export class ActionIconsComponent {
+	constructor(private actionItemService: ActionIconService) { }
+	get actionItems(): ActionIcon[] {
+		return this.actionItemService.getActionIcons();
+	}
+	get showMoreMenu() {
+		return this.actionItemService.getActionIcons().find((actionItem: ActionIcon): boolean => {
+			return actionItem.showAsAction != true;
+		})
+	}
+}
+
+@NgModule({
+	declarations: [
+		ActionIconsComponent
+	],
+	exports: [
+		ActionIconsComponent
+	],
+	imports: [
+		CommonModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatIconModule,
+		MatMenuModule,
+		MatTooltipModule,
+		RouterModule
+	],
+	providers: [
+		ActionIconService
+	]
+})
+export class ActionIconsModule { }
