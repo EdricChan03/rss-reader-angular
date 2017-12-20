ee2fc99a03239bbafe919e1ea3aeef9f278e1336
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 00:43:56 +0800

    chore(styles): Add style url for android page

--- src/app/devices/android/android.component.ts
+++ src/app/devices/android/android.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-android',
-	templateUrl: './android.component.html'
+	templateUrl: './android.component.html',
+	styleUrls: ['./android.component.scss']
 })
 export class AndroidComponent implements OnInit {
 
