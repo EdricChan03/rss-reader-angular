946deec8d28aef5509387ec10fe2b65e21862b32
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 01:30:21 +0800

    chore: Add home icon to not found page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ActionIcon, ActionIconService } from '../actionitem.service';
 
 @Component({
 	selector: 'app-page-not-found',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-	constructor(public router: Router) { }
+	constructor(public router: Router, private actionItemService: ActionIconService) { }
 
 	ngOnInit() {
-	}
+		this.actionItemService.addActionIcon({title: "Go back", icon: "arrow_back", onClickListener: ()=> {
+			window.history.back();
+		}, showAsAction: true});
+	};
 
 }
