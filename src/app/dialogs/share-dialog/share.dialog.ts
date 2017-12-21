d789866859eca5f1aaaa6c3a2c570c77e493b4d8
Edric Chan <edric.chan.1997@gmail.com>
2017-12-21 11:35:09 +0800

    feat: Implement share dialog

--- src/app/dialogs/share-dialog/share.dialog.ts
+++ src/app/dialogs/share-dialog/share.dialog.ts
@@ -1,30 +1,42 @@
 import { MatDialogRef } from '@angular/material';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'share-dialog',
-	templateUrl: './share.dialog.html'
+	templateUrl: './share.dialog.html',
+	styleUrls: ['./share.dialog.scss']
 })
-export class ShareDialog {
-	constructor(private dialogRef: MatDialogRef<ShareDialog>){
+export class ShareDialog implements OnInit {
+	url: string;
+	feed: any;
+	constructor(private dialogRef: MatDialogRef<ShareDialog>) {
 		dialogRef.disableClose = true;
 	}
-	shareNative() {
-		/**
-		 * @todo
-		 */
-		// if (navigator.share !== undefined) {
-		// 	navigator.share({
-		// 		title: document.title,
-		// 		text: "Check out this page about\"" + document.title + "\"!",
-		// 		url: document.URL
-		// 	}).then(()=> console.log("Successfully shared current page."))
-		// 	   .catch((error) => console.error("Error sharing current page: ", error));
-		// } else {
-		// 	alert("To share natively, please enable the 'Experimental Web Platform Features' flag in chrome://flags. Otherwise, it might not be supported on your platform right now.")
-		// }
+	ngOnInit() {
+		this.url = this.feed.link;
 	}
-	feed: any;
+	shareToFacebook() {
+
+	}
+	shareToTwitter() {
+		
+	}
+	shareToGooglePlus() {
+		
+	}
+	shareNative() {
+		if (navigator.share !== undefined) {
+			navigator.share({
+				title: document.title,
+				text: 'Check out this page about"' + document.title + '"!',
+				url: document.URL
+			}).then(() => console.log('Successfully shared current page.'))
+				.catch((error) => console.error('Error sharing current page: ', error));
+		} else {
+			// tslint:disable-next-line:max-line-length
+			alert('To share natively, please enable the \'Experimental Web Platform Features\' flag in chrome://flags. Otherwise, it might not be supported on your platform right now.');
+		}
+	}
 	cancel() {
 		this.dialogRef.close();
 	}
