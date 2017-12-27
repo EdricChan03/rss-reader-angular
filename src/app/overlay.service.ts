cbf6d17d13824e08b298ba701ca562bffebcb56e
Edric Chan <edric.chan.1997@gmail.com>
2017-12-27 22:33:03 +0800

    feat(overlay-service): Create methods that create a position strategy and accept parameters - Also add method for getting overlay ref

--- src/app/overlay.service.ts
+++ src/app/overlay.service.ts
@@ -1,6 +1,6 @@
 import { ComponentPortal } from '@angular/cdk/portal';
-import { OverlayRef, Overlay, OverlayConfig } from '@angular/cdk/overlay';
-import { Injectable } from '@angular/core';
+import { OverlayRef, Overlay, OverlayConfig, GlobalPositionStrategy, OriginConnectionPosition, OverlayConnectionPosition, ConnectedPositionStrategy } from '@angular/cdk/overlay';
+import { Injectable, ElementRef } from '@angular/core';
 
 @Injectable()
 export class OverlayService {
@@ -21,17 +21,50 @@ export class OverlayService {
 	 * Destroys the currently opened overlay
 	 */
 	destroyOverlay(): void {
-		if (this._overlayRef.hasAttached()) {
+		if (this._overlayRef.hasAttached() && this._overlayRef !== null) {
 			this._overlayRef.dispose();
 		}
 	}
 	/**
-	 * Closes the overlay currently opened
+	 * Closes the overlay currently opened (alias)
 	 * @see {@link OverlayService#destroyOverlay}
 	 */
 	close(): void {
 		this.destroyOverlay();
 	}
+	getOverlayRef(): OverlayRef | void {
+		if (this._overlayRef) {
+			return this._overlayRef;
+		}
+	}
+	/**
+	 * Creates a center overlay position strategy
+	 * @returns {GlobalPositionStrategy} The position strategy
+	 */
+	createCenterOverlayPositionStrategy(): GlobalPositionStrategy {
+		return this._overlay
+			.position()
+			.global()
+			.centerHorizontally()
+			.centerVertically();
+	}
+	/**
+	 * Creates a below the position of an element position strategy
+	 * @param {ElementRef} elementRef The element ref
+	 * @param {OriginConnectionPosition} originPos The origin's positions
+	 * @param {OverlayConnectionPosition} overlayPos The overlay's positions
+	 * @returns {ConnectedPositionStrategy} The position strategy
+	 */
+	createBelowPosElPositionStrategy(
+		elementRef: ElementRef,
+		originPos: OriginConnectionPosition,
+		overlayPos: OverlayConnectionPosition
+	): ConnectedPositionStrategy {
+		return this._overlay
+			.position()
+			.connectedTo(elementRef, originPos, overlayPos);
+	}
+
 	/**
 	 * Creates an overlay with the specified parameters
 	 * @param {ComponentPortal<any>} portal The portal to attach to the overlay's ref. For more info, visit {@link Overlay#create}
@@ -42,7 +75,7 @@ export class OverlayService {
 		this._overlayRef = this._overlay.create(config);
 		this._componentPortal = portal;
 		this._attachPortal();
-		if (backdropClickClosesOverlay) {
+		if (backdropClickClosesOverlay && config.hasBackdrop) {
 			this._overlayRef.backdropClick().subscribe(() => {
 				this.close();
 			});
