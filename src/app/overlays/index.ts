7d614fd0e5fa695e37f9b2a5fabeac0d6d99c8b1
Edric Chan <edric.chan.1997@gmail.com>
2017-12-28 14:42:57 +0800

    chore: Add index file

---
+++ src/app/overlays/index.ts
@@ -0,0 +1,4 @@
+export * from './apps-overlay/apps-overlay.component';
+export * from './filter-overlay/filter-overlay.component';
+export * from './notifications-overlay/notifications-overlay.component';
+export * from './onboarding-overlay/onboarding-overlay.component';
