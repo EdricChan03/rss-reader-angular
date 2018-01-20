a499a331b337e299dd9c10607562a5d3ad497055
Edric Chan <edric.chan.1997@gmail.com>
2018-01-20 18:49:56 +0800

    chore: add license

--- src/app/NavigatorShare.ts
+++ src/app/NavigatorShare.ts
@@ -1,4 +1,15 @@
 /**
+ * The following file is a wrapper for the native {@link Navigator#share} which for some reason isn't included.
+ * @license
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Edric Chan All rights reserved.
+ *
+ * Use of this source code is protected by a MIT-style license that can be found on the LICENSE file available at the root of this repository.
+ */
+
+/**
  * Parameters for {@link Navigator#share}
  */
 interface NavigatorShareParams {
@@ -16,5 +27,5 @@ interface NavigatorShareParams {
 	url?: string;
 }
 interface Navigator {
-	share(opts: NavigatorShareParams): Promise<any>;
+	share(opts: NavigatorShareParams): Promise<void>;
 }
