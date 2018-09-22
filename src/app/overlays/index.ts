7753f4066ade1a0f9de187666877dd48931e113d
Edric Chan <edric.chan.1997@gmail.com>
2018-09-22 09:51:14 +0800

    chore(overlay): remove apps overlay export

--- src/app/overlays/index.ts
+++ src/app/overlays/index.ts
@@ -1,4 +1,3 @@
-export * from './apps-overlay/apps-overlay.component';
 export * from './filter-overlay/filter-overlay.component';
 export * from './notifications-overlay/notifications-overlay.component';
 export * from './onboarding-overlay/onboarding-overlay.component';
