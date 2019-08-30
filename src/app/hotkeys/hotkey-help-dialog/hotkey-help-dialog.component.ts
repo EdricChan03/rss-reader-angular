b9a69ced0488ecfcf082da95e2226b240442f880
Edric Chan <edric.chan.1997@gmail.com>
2019-08-30 22:45:16 +0800

    feat(hotkeys/hotkey-help-dialog): show individual keys

--- src/app/hotkeys/hotkey-help-dialog/hotkey-help-dialog.component.ts
+++ src/app/hotkeys/hotkey-help-dialog/hotkey-help-dialog.component.ts
@@ -9,4 +9,69 @@ export class HotkeyHelpDialogComponent {
   hotkeys = Array.from(this.data);
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: Map<string, string>) {}
+
+  getDisplayedShortcut(keys: string): string[] {
+    const result = [];
+    const keysArray = keys.split('.');
+    // tslint:disable-next-line: forin
+    for (const key of keysArray) {
+      if (keysArray.includes(key)) {
+        let tempResult = key;
+        switch (key) {
+          case 'enter':
+            tempResult = 'Enter';
+            break;
+          case 'esc':
+            tempResult = 'Esc';
+            break;
+          case 'shift':
+            tempResult = 'Shift';
+            break;
+          case 'control':
+            tempResult = 'Control';
+            break;
+          case 'alt':
+            // The Alt key is an Option key on macOS devices
+            tempResult = window.navigator.platform === 'MacIntel' ? 'Option' : 'Alt';
+            break;
+          case 'meta':
+            tempResult = 'Command';
+            break;
+          case 'tab':
+            tempResult = 'Tab';
+            break;
+          case 'backspace':
+            tempResult = 'Backspace';
+            break;
+          case 'arrowup':
+          case 'arrowdown':
+          case 'arrowleft':
+          case 'arrowright':
+            switch (key) {
+              case 'arrowup':
+                tempResult = '↑';
+                break;
+              case 'arrowdown':
+                tempResult = '↓';
+                break;
+              case 'arrowleft':
+                tempResult = '←';
+                break;
+              case 'arrowright':
+                tempResult = '→';
+                break;
+            }
+            break;
+          default:
+            if (key.startsWith('f') && key.length === 2) {
+              tempResult = `F${key[1]}`;
+            } else {
+              tempResult = key;
+            }
+        }
+        result.push(tempResult);
+      }
+    }
+    return result;
+  }
 }
