f89ead2ed3a06d8043e5edcf130e1024ffa58c92
Edric Chan <edric.chan.1997@gmail.com>
2019-08-30 22:44:31 +0800

    fix(about-dialog): add missing reference

--- src/app/dialogs/about-dialog/about-dialog.component.ts
+++ src/app/dialogs/about-dialog/about-dialog.component.ts
@@ -2,11 +2,14 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 import { Component } from '@angular/core';
 
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-about-dialog',
   templateUrl: './about-dialog.component.html'
 })
 export class AboutDialogComponent {
+
+  latestVersion = environment.latestVersion;
 
   constructor(
     private dialog: MatDialog,
