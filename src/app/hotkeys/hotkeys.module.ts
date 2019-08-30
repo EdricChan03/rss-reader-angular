515b235a679cbf210feaf664d22a71eb4c5f6177
Edric Chan <edric.chan.1997@gmail.com>
2019-08-30 22:45:29 +0800

    refactor(hotkeys.module): restructure imports

--- src/app/hotkeys/hotkeys.module.ts
+++ src/app/hotkeys/hotkeys.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HotkeysService } from './hotkeys.service';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { HotkeyHelpDialogComponent } from './hotkey-help-dialog/hotkey-help-dialog.component';
+import { HotkeysService } from './hotkeys.service';
 
 @NgModule({
   declarations: [HotkeyHelpDialogComponent],
