3342530f02e92c47478debe6b503a78ed45cc915
Edric <edric.chan.1997@gmail.com>
2019-12-29 01:30:45 +0800

    refactor(truncate): use Unicode ellipsis instead of three dots

--- projects/rss-reader/src/app/pipe/truncate.pipe.ts
+++ projects/rss-reader/src/app/pipe/truncate.pipe.ts
@@ -8,7 +8,7 @@ export class TruncatePipe implements PipeTransform {
 
 transform(value: string, args: string[]): string {
     const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
-    const trail = args.length > 1 ? args[1] : '...';
+    const trail = args.length > 1 ? args[1] : '&hellip;';
     return value.length > limit ? value.substring(0, limit) + trail : value;
    }
 }
