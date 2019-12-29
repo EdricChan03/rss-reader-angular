294adcb1f287b1079d0b270a185773421295d1ff
Edric <edric.chan.1997@gmail.com>
2019-12-29 23:53:42 +0800

    ci: add full support for testing

--- projects/rss-reader/karma.conf.js
+++ projects/rss-reader/karma.conf.js
@@ -12,15 +12,14 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, '../../coverage/rss-reader'),
+      // See https://istanbul.js.org/docs/advanced/alternative-reporters/ for more info.
+      reports: ['html', 'json-summary', 'text-summary'],
       fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -28,6 +27,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
