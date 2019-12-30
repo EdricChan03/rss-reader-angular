1262230bf2a436a92bbc30c5234260774e4d656c
Edric <edric.chan.1997@gmail.com>
2019-12-30 12:38:32 +0800

    chore(hotkey-help-dialog): improve logic for `#getDisplayedShortcut`

--- projects/rss-reader/src/app/hotkeys/hotkey-help-dialog/hotkey-help-dialog.component.ts
+++ projects/rss-reader/src/app/hotkeys/hotkey-help-dialog/hotkey-help-dialog.component.ts
@@ -19,16 +19,12 @@ export class HotkeyHelpDialogComponent {
         let tempResult = key;
         switch (key) {
           case 'enter':
-            tempResult = 'Enter';
-            break;
           case 'esc':
-            tempResult = 'Esc';
-            break;
           case 'shift':
-            tempResult = 'Shift';
-            break;
           case 'control':
-            tempResult = 'Control';
+          case 'tab':
+          case 'backspace':
+            tempResult = this.capitalise(key);
             break;
           case 'alt':
             // The Alt key is an Option key on macOS devices
@@ -36,12 +32,6 @@ export class HotkeyHelpDialogComponent {
             break;
           case 'meta':
             tempResult = 'Command';
-            break;
-          case 'tab':
-            tempResult = 'Tab';
-            break;
-          case 'backspace':
-            tempResult = 'Backspace';
             break;
           case 'arrowup':
           case 'arrowdown':
@@ -64,14 +54,20 @@ export class HotkeyHelpDialogComponent {
             break;
           default:
             if (key.startsWith('f') && (key.length === 2 || key.length === 3)) {
-              tempResult = `F${key[1]}`;
+              // See https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/ for more info
+              tempResult = this.capitalise(key);
             } else {
-              tempResult = key;
+              tempResult = key.toLocaleLowerCase();
             }
         }
         result.push(tempResult);
       }
     }
     return result;
+  }
+
+  private capitalise(str: string): string {
+    // See https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/
+    return str.charAt(0).toLocaleUpperCase() + str.substring(1);
   }
 }
