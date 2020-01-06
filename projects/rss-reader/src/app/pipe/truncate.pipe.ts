18f1d0fa8809ba10ff27b70a0dc779f8470afa2d
Edric <edric.chan.1997@gmail.com>
2020-01-07 02:16:50 +0800

    chore(TruncatePipe): fix indenting

--- projects/rss-reader/src/app/pipe/truncate.pipe.ts
+++ projects/rss-reader/src/app/pipe/truncate.pipe.ts
@@ -1,14 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
- name: 'truncate'
+  name: 'truncate'
 })
-
 export class TruncatePipe implements PipeTransform {
 
-transform(value: string, args: string[]): string {
+  transform(value: string, ...args: any[]): string {
     const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
     const trail = args.length > 1 ? args[1] : '&hellip;';
     return value.length > limit ? value.substring(0, limit) + trail : value;
-   }
+  }
 }
