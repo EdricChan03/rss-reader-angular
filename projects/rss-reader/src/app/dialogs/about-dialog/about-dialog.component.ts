45731c4b149701953ff63ab1b660f0fcda92d520
Edric <edric.chan.1997@gmail.com>
2020-01-16 00:49:23 +0800

    refactor(about-dialog): remove unused dependencies, methods and imports

--- projects/rss-reader/src/app/dialogs/about-dialog/about-dialog.component.ts
+++ projects/rss-reader/src/app/dialogs/about-dialog/about-dialog.component.ts
@@ -1,22 +1,10 @@
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-
 import { Component } from '@angular/core';
-
 import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-about-dialog',
   templateUrl: './about-dialog.component.html'
 })
 export class AboutDialogComponent {
-
   latestVersion = environment.latestVersion.full;
-
-  constructor(
-    private dialog: MatDialog,
-    private dialogRef: MatDialogRef<AboutDialogComponent>
-  ) { }
-  // TODO: Implement this method
-  openVersionDialog() {
-
-  }
 }
