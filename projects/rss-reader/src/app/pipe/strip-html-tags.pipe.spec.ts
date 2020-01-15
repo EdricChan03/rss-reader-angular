80845ae54cec03f9c37d73a2ac529bdc77130e68
Edric <edric.chan.1997@gmail.com>
2020-01-16 01:22:22 +0800

    test(strip-html-tags): add additional tests

--- projects/rss-reader/src/app/pipe/strip-html-tags.pipe.spec.ts
+++ projects/rss-reader/src/app/pipe/strip-html-tags.pipe.spec.ts
@@ -1,11 +1,64 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
 import { StripHtmlTagsPipe } from './strip-html-tags.pipe';
 
-describe('Pipe: StripHtmlTags', () => {
-  it('create an instance', () => {
+// Adapted from
+// https://github.com/zspecza/common-tags/blob/master/src/oneLine/oneLine.js
+function removeWhitespace(str: string) {
+  return str.replace(/^\s+|\s+$/gm, '').split('\n').join('');
+}
+
+describe('StripHtmlTagsPipe', () => {
+  it('should create an instance', () => {
     const pipe = new StripHtmlTagsPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should strip all tags', () => {
+    const html = removeWhitespace(`<p>Hello, paragraph tag!</p>
+    <em>Emphasised text</em>`);
+    const expectedHtml = 'Hello, paragraph tag!Emphasised text';
+
+    const pipe = new StripHtmlTagsPipe();
+    const value = pipe.transform(html);
+
+    expect(value).toEqual(expectedHtml, 'Expected all HTML tags to be stripped');
+  });
+
+  it('should strip comments', () => {
+    const html = removeWhitespace(`<!-- HTML comment -->
+    <p>Hello, HTML!</p>
+    <!--
+    This is a comment
+    that spans multiple lines
+    -->`);
+    const expectedHtml = 'Hello, HTML!';
+
+    const pipe = new StripHtmlTagsPipe();
+    const value = pipe.transform(html);
+
+    expect(value).toEqual(expectedHtml, 'Expected HTML comments to be stripped');
+  });
+
+  it('should strip <script> tags', () => {
+    const html = removeWhitespace(`<script>console.log('Hello XSS!');</script>
+    <p>Hello, HTML content!</p>`);
+    const expectedHtml = 'Hello, HTML content!';
+
+    const pipe = new StripHtmlTagsPipe();
+    const value = pipe.transform(html);
+
+    expect(value).toEqual(expectedHtml, 'Expected HTML <script> tags to be stripped');
+  });
+
+  it('should strip <style> tags', () => {
+    const html = removeWhitespace(`<style>p{color:red;font-family:Arial;}</style>
+    <p>Hello, HTML content!</p>`);
+    const expectedHtml = 'Hello, HTML content!';
+
+    const pipe = new StripHtmlTagsPipe();
+    const value = pipe.transform(html);
+
+    expect(value).toEqual(expectedHtml, 'Expected HTML <style> tags to be stripped');
   });
 });
