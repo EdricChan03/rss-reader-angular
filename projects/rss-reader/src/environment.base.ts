e275c332fbf416d50f71ebd5b458e2c9e8a1e0bb
Edric <edric.chan.1997@gmail.com>
2020-01-16 00:44:46 +0800

    chore(environment.base): remove Firebase app config
    The app had been deleted for while but the config wasn't removed.

--- projects/rss-reader/src/environment.base.ts
+++ projects/rss-reader/src/environment.base.ts
@@ -6,15 +6,6 @@ import { latestVersion as versionJson } from './assets/release-notes/release-not
 // This file contains the constants that all of the environments
 // that the app defines.
 
-/** Firebase app configuration */
-export interface FirebaseAppConfig {
-  apiKey?: string;
-  authDomain?: string;
-  databaseURL?: string;
-  projectId?: string;
-  storageBucket?: string;
-  messagingSenderId?: string;
-}
 /** Environment interface */
 export interface Environment {
   /** The app's latest version. */
@@ -28,24 +19,12 @@ export interface Environment {
    * module.
    */
   routerOptions?: ExtraOptions;
-  /** Firebase configuration options. (Currently unused) */
-  firebaseConfig?: FirebaseAppConfig;
   /** Default Git repo options to be used in-place for the release notes options. */
   gitRepoDefaults?: GitRepo;
 }
 
 /** The latest version of the app. */
 export const latestVersion = new Version(versionJson);
-
-/** The Firebase configuration. */
-export const firebaseConfig: FirebaseAppConfig = {
-  apiKey: 'AIzaSyB70FnAVNaxg33vyY4tlPKZ5x00QY80mTc',
-  authDomain: 'rss-reader-a5b4b.firebaseapp.com',
-  databaseURL: 'https://rss-reader-a5b4b.firebaseio.com',
-  projectId: 'rss-reader-a5b4b',
-  storageBucket: '',
-  messagingSenderId: '870397667989'
-};
 
 /** Default configuration for the Git repository. */
 export const gitRepoDefaults: GitRepo = {
