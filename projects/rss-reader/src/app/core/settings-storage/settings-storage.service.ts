8acd9128ad9d91ef4883a3627520105463c27846
Edric <edric.chan.1997@gmail.com>
2020-02-08 02:18:41 +0800

    feat(settings-storage): add support for configuring the behaviour of the service
    The service now allows for specifying what default settings to be returned and when if no such value exists in the storage.

--- projects/rss-reader/src/app/core/settings-storage/settings-storage.service.ts
+++ projects/rss-reader/src/app/core/settings-storage/settings-storage.service.ts
@@ -1,29 +1,56 @@
-import { Injectable, Inject, InjectionToken } from '@angular/core';
+import { Injectable, Inject, InjectionToken, Optional } from '@angular/core';
 import { Settings } from '../../models/settings';
 
-/** The settings provider to use. (Default: `localStorage`) */
-export const SETTINGS_PROVIDER: InjectionToken<Storage> = new InjectionToken('Settings storage provider', {
+/** Configuration options for {@link SettingsStorageService}. */
+export interface SettingsStorageConfig<S = Settings> {
+  /** Whether to revert to the default settings if no such value exists in storage. */
+  canRevertDefault?: boolean;
+  /** Default settings to be used if no such value exists in storage. */
+  defaultSettings?: S;
+}
+
+/** The settings storage provider to use. (Default: `localStorage`) */
+export const SETTINGS_STORAGE_PROVIDER: InjectionToken<Storage> = new InjectionToken('Settings storage provider', {
   factory: () => localStorage
 });
 
 /** The settings key to store the settings in. (Default: `settings`) */
-export const SETTINGS_KEY: InjectionToken<string> = new InjectionToken('Settings storage key', {
+export const SETTINGS_STORAGE_KEY: InjectionToken<string> = new InjectionToken('Settings storage key', {
   factory: () => 'settings'
+});
+
+// Note: Turns out you need a function in order to return an object for a factory.
+function SETTINGS_STORAGE_DEFAULT_CONFIG_FACTORY(): SettingsStorageConfig {
+  return {
+    canRevertDefault: true,
+    defaultSettings: {
+      openNewTab: true,
+      showImages: true,
+      theme: 'indigo-pink',
+      notifyNewReleases: true
+    }
+  };
+}
+
+/** Configuration options to change the behavior of {@link SettingsStorageService}. */
+export const SETTINGS_STORAGE_CONFIG: InjectionToken<SettingsStorageConfig> = new InjectionToken('Settings storage config', {
+  factory: SETTINGS_STORAGE_DEFAULT_CONFIG_FACTORY
 });
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsStorageService<S = Settings> {
-
   constructor(
-    @Inject(SETTINGS_PROVIDER) private storage: Storage,
-    @Inject(SETTINGS_KEY) private key: string
+    @Inject(SETTINGS_STORAGE_PROVIDER) private storage: Storage,
+    @Inject(SETTINGS_STORAGE_KEY) private key: string,
+    @Inject(SETTINGS_STORAGE_CONFIG) @Optional() private config: SettingsStorageConfig<S>
   ) { }
 
   /** Retrieves the current settings (as JSON). */
   get settings(): S {
-    return JSON.parse(this.storage.getItem(this.key)) as S;
+    return (this.storage.getItem(this.key) && this.config.canRevertDefault) ?
+    JSON.parse(this.storage.getItem(this.key)) as S : this.config.defaultSettings;
   }
 
   /** Overrides the current settings. */
