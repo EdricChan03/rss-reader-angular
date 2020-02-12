826173c7e7831836da9b1d8797be50a4cb69763f
Edric <edric.chan.1997@gmail.com>
2020-02-12 18:23:17 +0800

    feat(article-card): remove need for different templates

--- projects/rss-reader/src/app/components/article-card/article-card.component.ts
+++ projects/rss-reader/src/app/components/article-card/article-card.component.ts
@@ -4,6 +4,7 @@ import { CodeViewerDialogComponent, ShareDialogComponent } from '../../dialogs';
 import { FeedEntry } from '../../models/feed-entry';
 import { NewsAPITopHeadlinesArticle } from '../../models/news-api/top-headlines-article';
 import { SettingsStorageService } from '../../core/settings-storage/settings-storage.service';
+import { StripHtmlTagsPipe } from '../../pipe';
 
 @Component({
   selector: 'app-article-card',
@@ -29,8 +30,75 @@ export class ArticleCardComponent implements OnInit {
 
   constructor(
     private dialog: MatDialog,
-    private settingsStorage: SettingsStorageService
+    private settingsStorage: SettingsStorageService,
+    private stripHtmlTags: StripHtmlTagsPipe
   ) { }
+
+  get articleAuthor(): string {
+    if ('publishedAt' in this.article) {
+      if ('author' in this.article && this.article.author !== null) {
+        if ('author' in this.article && 'source' in this.article &&
+        'name' in this.article.source && this.article.source.name !== null) {
+          return `${this.article.author} | ${this.article.source.name}`;
+        } else {
+          return this.article.author;
+        }
+      } else if ('source' in this.article &&
+      'name' in this.article.source && this.article.source.name !== null) {
+        return this.article.source.name;
+      } else {
+        return;
+      }
+    } else if ('pubDate' in this.article) {
+      return this.article.author;
+    }
+    return;
+  }
+
+  get articlePubDate(): string | Date {
+    if ('publishedAt' in this.article) {
+      return this.article.publishedAt;
+    } else if ('pubDate' in this.article) {
+      return this.article.pubDate;
+    }
+    return;
+  }
+
+  get articleImg(): string {
+    if ('urlToImage' in this.article) {
+      return this.article.urlToImage;
+    } else if ('thumbnail' in this.article) {
+      return this.article.thumbnail;
+    } else if ('enclosure' in this.article && 'thumbnail' in this.article.enclosure) {
+      return this.article.enclosure.thumbnail;
+    } else {
+      return;
+    }
+  }
+
+  get articleTitle(): string {
+    return this.article.title;
+  }
+
+  get articleDesc(): string {
+    return this.stripHtmlTags.transform(this.article.description || '');
+  }
+
+  get articleCategories(): string[] {
+    if ('categories' in this.article) {
+      return this.article.categories;
+    }
+    return [];
+  }
+
+  get articleLink(): string {
+    if ('url' in this.article) {
+      return this.article.url;
+    } else if ('link' in this.article) {
+      return this.article.link;
+    }
+    return;
+  }
 
   ngOnInit() {
     if (this.settingsStorage.settings) {
@@ -42,28 +110,25 @@ export class ArticleCardComponent implements OnInit {
       }
     }
 
-    if ('pubDate' in this.article) {
-      // Article is of type FeedEntry.
-      // tslint:disable-next-line:deprecation
-      this.feed = this.article as FeedEntry;
-    } else if ('publishedAt' in this.article) {
-      // Article is of type NewsAPITopHeadlinesArticle.
-      // tslint:disable-next-line:deprecation
-      this.headline = this.article as NewsAPITopHeadlinesArticle;
-    } else {
-      throw new Error('The article passed in was not of type "FeedEntry" or "NewsAPITopHeadlinesArticle".');
+    // Deprecated usages
+    // tslint:disable:deprecation
+    if (this.feed) {
+      this.article = this.feed;
+    } else if (this.headline) {
+      this.article = this.headline;
     }
+    // tslint:enable:deprecation
   }
 
-  share(article: FeedEntry | NewsAPITopHeadlinesArticle) {
+  share() {
     this.dialog.open(ShareDialogComponent, {
-      data: article
+      data: this.article
     });
   }
 
-  showCode(article) {
+  showCode() {
     this.dialog.open(CodeViewerDialogComponent, {
-      data: article
+      data: this.article
     });
   }
 }
