f595650dd3e577a52b73ee84670f9f5bbd92e9b1
Edric Chan <edric.chan.1997@gmail.com>
2020-08-21 18:02:01 +0800

    feat(app/storybook): add support for app theming

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
-  "stories": [
-    "../projects/rss-reader/src/app/**/*.stories.mdx",
-    "../projects/rss-reader/src/app/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../projects/rss-reader/src/app/**/*.stories.mdx',
+    '../projects/rss-reader/src/app/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  'addons': [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false
+      }
+    },
+    'storybook-addon-themes'
   ]
-}
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,31 @@
 
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  themes: [
+    {
+      name: 'indigo-pink',
+      class: ['mat-app-background', 'indigo-pink'],
+      color: '#3f51b5',
+      default: true
+    },
+    {
+      name: 'deeppurple-amber',
+      class: ['mat-app-background', 'deeppurple-amber'],
+      color: '#673ab7'
+    },
+    {
+      name: 'pink-bluegrey',
+      class: ['mat-app-background', 'pink-bluegrey'],
+      color: '#e91e63'
+    },
+    {
+      name: 'purple-green',
+      class: ['mat-app-background', 'purple-green'],
+      color: '#9c27b0'
+    }
+  ]
 }
