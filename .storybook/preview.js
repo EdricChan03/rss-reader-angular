166b6812883267a8b3aad1836ec9eb94dcfb983c
Edric Chan <edric.chan.1997@gmail.com>
2020-08-22 01:09:50 +0800

    feat(app/storybook): enable expanded mode for Storybook controls

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,6 +8,7 @@ import { themes } from '@storybook/theming';
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   docs: { theme: themes.dark },
+  controls: { expanded: true },
   themes: [
     {
       name: 'indigo-pink',
