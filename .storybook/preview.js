ef9a6bdf78a5dab3d01308ec2673ea64578e5b47
Edric Chan <edric.chan.1997@gmail.com>
2020-08-23 23:27:39 +0800

    refactor(app/storybook): improve theming + add additional height to docs iframes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,28 +7,31 @@ import { themes } from '@storybook/theming';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  docs: { theme: themes.dark },
+  docs: {
+    theme: themes.dark,
+    iframeHeight: '400px'
+  },
   controls: { expanded: true },
   themes: [
     {
       name: 'indigo-pink',
-      class: ['mat-app-background', 'indigo-pink'],
+      class: 'indigo-pink',
       color: '#3f51b5',
       default: true
     },
     {
       name: 'deeppurple-amber',
-      class: ['mat-app-background', 'deeppurple-amber'],
+      class: 'deeppurple-amber',
       color: '#673ab7'
     },
     {
       name: 'pink-bluegrey',
-      class: ['mat-app-background', 'pink-bluegrey'],
+      class: 'pink-bluegrey',
       color: '#e91e63'
     },
     {
       name: 'purple-green',
-      class: ['mat-app-background', 'purple-green'],
+      class: 'purple-green',
       color: '#9c27b0'
     }
   ]
