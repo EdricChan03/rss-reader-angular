932493198a9218bde339fff3d43bd8584d23cae1
Edric <edric.chan.1997@gmail.com>
2024-09-03 12:09:19 +0800

    chore: add ESLint stylistic plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,56 @@
 // @ts-check
 
 // Allows us to bring in the recommended core rules from eslint itself
-const eslint = require("@eslint/js");
+import eslint from "@eslint/js";
+const { configs } = eslint;
 
 // Allows us to use the typed utility for our config, and to bring in the recommended rules for TypeScript projects from typescript-eslint
-const tseslint = require("typescript-eslint");
+import { config, configs as tsConfigs } from "typescript-eslint";
 
 // Allows us to bring in the recommended rules for Angular projects from angular-eslint
-const angular = require("angular-eslint");
+import {
+  configs as angularConfigs,
+  processInlineTemplates,
+} from "angular-eslint";
+
+// Stylistic rules
+import stylistic from "@stylistic/eslint-plugin";
+
+const stylisticConfig = stylistic.configs.customize({
+  flat: true,
+  indent: 2,
+  quotes: "single",
+  semi: true,
+
+});
 
 // Export our config array, which is composed together thanks to the typed utility function from typescript-eslint
-module.exports = tseslint.config(
+export default config(
+  {
+    files: ["**/*.js"],
+    extends: [
+      stylisticConfig,
+      configs.recommended
+    ]
+  },
   {
     // Everything in this config object targets our TypeScript files (Components, Directives, Pipes etc)
     files: ["**/*.ts"],
     extends: [
+      stylisticConfig,
       // Apply the recommended core rules
-      eslint.configs.recommended,
+      configs.recommended,
       // Apply the recommended TypeScript rules
-      ...tseslint.configs.recommended,
+      ...tsConfigs.recommended,
       // Optionally apply stylistic rules from typescript-eslint that improve code consistency
-      ...tseslint.configs.stylistic,
+      ...tsConfigs.stylistic,
       // Apply the recommended Angular rules
-      ...angular.configs.tsRecommended,
+      ...angularConfigs.tsRecommended,
+      // stylistic.configs["recommended-flat"],
     ],
     // Set the custom processor which will allow us to have our inline Component templates extracted
     // and treated as if they are HTML files (and therefore have the .html config below applied to them)
-    processor: angular.processInlineTemplates,
+    processor: processInlineTemplates,
     // Override specific rules for TypeScript files (these will take priority over the extended configs above)
     rules: {
       "@angular-eslint/directive-selector": [
@@ -53,10 +77,10 @@ module.exports = tseslint.config(
     files: ["**/*.html"],
     extends: [
       // Apply the recommended Angular template rules
-      ...angular.configs.templateRecommended,
+      ...angularConfigs.templateRecommended,
       // Apply the Angular template rules which focus on accessibility of our apps
-      ...angular.configs.templateAccessibility,
+      ...angularConfigs.templateAccessibility
     ],
     rules: {},
-  }
+  },
 );
